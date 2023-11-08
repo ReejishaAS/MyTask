a3517a1ba6e178b5c567a9d9345dcda186942cb3
Reejishaas <reejishaas123@gmail.com>
2023-11-08 14:50:16 +0530

    Task 16

--- UserDefinedException.java
+++ UserDefinedException.java
@@ -7,19 +7,26 @@ class SeniorCitizenFactorException extends Exception {
 }
 
 public class UserDefinedException {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your age to check if you are a senior citizen:");
+        UserDefinedException userDefine = new UserDefinedException();
 
         try {
+            System.out.println("Enter your age to check if you are a senior citizen:");
             int age = sc.nextInt();
-            if (age < 60) {
-                throw new SeniorCitizenFactorException("You are not a senior citizen.");
-            }
+            userDefine.checkCitizen(age);
 
         } catch (SeniorCitizenFactorException e) {
             System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    public void checkCitizen(int age) throws SeniorCitizenFactorException {
+        if (age < 60) {
+            throw new SeniorCitizenFactorException("You are not a senior citizen.");
+        } else {
+            System.out.println("Your are a senior citizen");
         }
     }
 }

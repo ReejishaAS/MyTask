0089f4800612714cfe6b871ecea8a1b963503d46
Reejishaas <reejishaas123@gmail.com>
2023-11-08 17:33:16 +0530

    Task 21

--- ThreadCheck.java
+++ ThreadCheck.java
@@ -1,33 +1,32 @@
 class FirstThread extends Thread {
+    public FirstThread(String name) {
+        super(name);
+        start();
+    }
+
     public void run() {
         System.out.println("FirstThread is running...");
     }
 }
 
 class SecondThread extends Thread {
+    public SecondThread(String name) {
+        super(name);
+        start();
+    }
+
     public void run() {
         System.out.println("SecondThread is running...");
     }
 }
 
 public class ThreadCheck {
-    private FirstThread firstThread;
-    private SecondThread secondThread;
-
-    public ThreadCheck() {
-        firstThread = new FirstThread();
-        secondThread = new SecondThread();
-        firstThread.start();
-        secondThread.start();
-    }
-
-    public void checkThreads() {
-        System.out.println("Is first thread working? " + firstThread.isAlive());
-        System.out.println("Is second thread working? " + secondThread.isAlive());
-    }
 
     public static void main(String[] args) {
-        ThreadCheck threadCheck = new ThreadCheck();
-        threadCheck.checkThreads();
+        FirstThread firstThread = new FirstThread("First");
+        SecondThread secondThread = new SecondThread("Second");
+
+        System.out.println("Is first thread working? " + firstThread.isAlive());
+        System.out.println("Is second thread working? " + secondThread.isAlive());
     }
 }

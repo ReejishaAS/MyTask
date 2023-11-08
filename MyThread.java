b8194097a9dfc51fab86a14133ba37dd94770206
Reejishaas <reejishaas123@gmail.com>
2023-11-08 15:03:15 +0530

    Task 20

--- MyThread.java
+++ MyThread.java
@@ -1,19 +1,31 @@
-class MyRunnable implements Runnable {
+class Thread1 extends Thread {
     public void run() {
         try {
-            Thread.sleep(500); // Sleep for 500 milliseconds
+            Thread.sleep(500);
+            System.out.println("My First program is starting..");
+        } catch (InterruptedException e) {
+            System.out.println("First Thread interrupted.");
+        }
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        try {
+            Thread.sleep(500);
             System.out.println("My Second program is starting..");
         } catch (InterruptedException e) {
-            System.out.println("Thread interrupted.");
+            System.out.println(" Second Thread interrupted.");
         }
     }
 }
 
 public class MyThread {
     public static void main(String[] args) {
-        MyRunnable runnable = new MyRunnable();
-        Thread thread = new Thread(runnable);
-        thread.start();
-        System.out.println("My first program starting...");
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+        thread1.start();
+        thread2.start();
+        System.out.println("My program starting...");
     }
 }

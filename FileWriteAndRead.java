c08629759d839cdc27be448f6106f8817018ced3
Reejishaas <reejishaas123@gmail.com>
2023-11-23 11:47:54 +0530

    Task

--- FileWriteAndRead.java
+++ FileWriteAndRead.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class FileWriteAndRead {
     public static void main(String[] args) {
+        FileWriter fw = null; // Declare FileWriter outside the try block.
+        FileReader fr = null;
+
         try (Scanner sc = new Scanner(System.in)) {
             File testFile = new File("C:\\Users\\Reejisha\\Desktop\\Task\\Sample\\file.txt");
             testFile.createNewFile();
@@ -15,15 +18,20 @@ public class FileWriteAndRead {
                 // Writing
                 System.out.println("Enter text to write to the file:");
                 String sentence = sc.nextLine();
-
-                try (FileWriter fw = new FileWriter(testFile)) {
+                try {
+                    fw = new FileWriter(testFile);
                     fw.write(sentence);
                     System.out.println("Write successfully.");
+
+                } catch (IOException e) {
+                    System.out.println("Error occurred while writing the file.");
+                    e.printStackTrace();
                 }
 
                 if (testFile.canRead()) {
                     // Reading
-                    try (FileReader fr = new FileReader(testFile)) {
+                    try {
+                        fr = new FileReader(testFile);
                         int i;
                         while ((i = fr.read()) != -1)
                             System.out.print((char) i);
@@ -41,6 +49,18 @@ public class FileWriteAndRead {
         } catch (IOException e) {
             System.out.println("Error occurred.");
             e.printStackTrace();
+        } finally {
+            // Close the FileWriter in the finally block to ensure it gets closed even if an
+            // exception occurs.
+            try {
+                if ((fw != null) || (fr != null)) {
+                    fw.close();
+                    fr.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error occurred while closing FileWriter.");
+                e.printStackTrace();
+            }
         }
     }
 }

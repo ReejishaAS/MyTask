add5785b0f10797019205d8a52750e0768919f98
Reejishaas <reejishaas123@gmail.com>
2023-11-23 12:41:36 +0530

    Task

---
+++ CreateFile.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) throws IOException {
+
+        File directory = new File("C:\\Users\\Reejisha\\Desktop\\Task");
+        directory.mkdir(); // Create directory
+
+        File file = new File(directory, "testFile.txt");
+        try {
+
+            file.createNewFile(); // Create file
+            System.out.println("File name " + file.getName());
+            System.out.println("File created successfully.");
+
+        } catch (IOException e) {
+
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
